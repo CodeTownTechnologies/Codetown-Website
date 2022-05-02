90c123a61b0699a19229e3f87e32aea76f8c312d
gauravjainn <itsgauravjain.gj@gmail.com>
2022-05-02 16:56:20 +0530

    create footer and set alignment of homepage

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 import "../footer/footer.css";
 import Logo from "../footer/Logo.png";
 import BottomToTop from "../BottomToTop/BottomToTop";
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
+  const location = useLocation();
   return (
     <div style={{ marginTop: "100px" }}>
       <div className="mister_separator"></div>
@@ -11,129 +14,76 @@ const Footer = () => {
         <div id="footer-container">
           <div id="footer-grid">
             <div id="left-footer-section" className="footer-section">
-              <div id="footer-search">
-                <form action="">
-                  <input type="search" placeholder="Web design" />
-                  <button type="submit">Search</button>
-                </form>
-              </div>
-              <div className="footer-information">
-                <div className="footer-information">
-                  <p>
-                    <img
-                      src="https://img.icons8.com/ios-filled/12/999999/marker.png"
-                      alt=""
-                    />
-                    30/20, Verkhy street, Moscow, Russia
-                  </p>
-                  <p>
-                    <img
-                      src="https://img.icons8.com/ios-filled/12/999999/phone.png"
-                      alt=""
-                    />
-                    7 (800) 555–35–35
-                  </p>
-                  <p>
-                    <img
-                      src="https://img.icons8.com/ios-filled/12/999999/mail.png"
-                      alt=""
-                    />
-                    noreply@reply.io
-                  </p>
-                  <p>
-                    <img
-                      src="https://img.icons8.com/ios-filled/12/999999/clock.png"
-                      alt=""
-                    />
-                    8:00 AM – 8:00 PM
-                  </p>
-                </div>
+              <div className="right-navbar">
+                {" "}
+                <Link style={{ textDecoration: 'none' }} to="/about-us"><p className={location.pathname === '/about-us' ? "route-link-active" : "route-link"}>About </p></Link>
+                {" "}
+                <p className={location.pathname === '#' ? "route-link-active" : "route-link"}>Services</p>
+                <p className={location.pathname === '#' ? "route-link-active" : "route-link"}>Technology</p>
+                <p className={location.pathname === '#' ? "route-link-active" : "route-link"}>Portfolio</p>
+                <p className={location.pathname === '#' ? "route-link-active" : "route-link"}>Process</p>
+                <p className={location.pathname === '#' ? "route-link-active" : "route-link"}>Career</p>
               </div>
             </div>
-            <div id="second-footer-section" className="footer-section">
-              <div className="footer-links">
-                <h3>Categories</h3>
-                <ul>
-                  <li role="menuitem">
-                    <a href="#">Software Development</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">Web Development</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">Site maintainance</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">SEO</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">Mobile development</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">UI/UX</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div id="right-footer-section" className="footer-section">
-              <div className="footer-links">
-                <h3>Information</h3>
-                <ul>
-                  <li role="menuitem">
-                    <a href="#">Home</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">About</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">Contact Information</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">
-                      Terms of Use
-                      <br />& Legal Information
-                    </a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">Message Us</a>
-                  </li>
-                  <li role="menuitem">
-                    <a href="#">Leave a Feedback</a>
-                  </li>
-                </ul>
+            <div id="right-footer-section" className="footer-section-1">
+              <div id="social-buttons">
+                <img className='social-icon' src="https://img.icons8.com/ios-filled/25/999999/facebook--v1.png" alt="image12" />
+                <img className='social-icon' src="https://img.icons8.com/ios-filled/25/999999/telegram-app.png" alt="image12" />
+                <img className='social-icon' src="https://img.icons8.com/ios-filled/25/999999/pinterest--v1.png" alt="image12" />
+                <img className='social-icon' src="https://img.icons8.com/ios-filled/25/999999/instagram--v1.png" alt="image12" />
+                {/* <BottomToTop/> */}
               </div>
             </div>
           </div>
-          <div id="copyright-information">
-            <div id="footer-logo-section">
+          <div id="copyright-information" style={{ width: '100%' }}>
+            <div>
+              <img
+                style={{
+                  height: "25px",
+                  width: "150px",
+                  ObjectFit: "contain",
+                }}
+                height="25px"
+                width="150px"
+                src={Logo}
+                alt=""
+              />
+            </div>
+            <div style={{ width: '100%' }}>
+              <div id="copyright-text" style={{ marginLeft: '15px' }}>
+                2020-2022 &copy;codetowntechnologies - All rights reserved
+              </div>
+            </div>
+            <div style={{ textAlign: 'right', justifyContent: 'end', float: 'right', width: '100%', display: 'flex' }}>
+              <div id="copyright-text-1" style={{ display: 'flex', marginRight: '10px' }}>
+                <img style={{ width: '20px', height: '20px', marginTop: '10px', marginRight: '3px' }} src="https://img.icons8.com/ios-filled/25/999999/facebook--v1.png" alt="image12" /> <p>+91 76270 37613</p>
+              </div>
+              <div id="copyright-text-1" style={{ display: 'flex' }}>
+                <img style={{ width: '20px', height: '20px', marginTop: '10px', marginRight: '3px' }} src="https://img.icons8.com/ios-filled/25/999999/facebook--v1.png" alt="image12" /> <p>hr@codetowntechnologies.com</p>
+              </div>
+            </div>
+            {/* <div id="footer-logo-section">
               <div id="footer-logo">
                 <img
                   style={{
-                    height: "30px",
-                    width: "70px",
+                    height: "25px",
+                    width: "150px",
                     ObjectFit: "contain",
                   }}
-                  height="30px"
-                  width="40px"
+                  height="25px"
+                  width="150px"
                   src={Logo}
                   alt=""
                 />
               </div>
-            </div>
-            <div id="copyright-text">
-              &copy; Reimu Inc. 2022. All rights reserved.
-            </div>
-            <div id="social-buttons">
-              <img src="https://img.icons8.com/ios-filled/25/999999/facebook--v1.png" alt="image12" />
-              <img src="https://img.icons8.com/ios-filled/25/999999/telegram-app.png" alt="image12" />
-              <img src="https://img.icons8.com/ios-filled/25/999999/pinterest--v1.png"alt="image12" />
-              <img src="https://img.icons8.com/ios-filled/25/999999/instagram--v1.png"alt="image12" />
-              <BottomToTop/>
-            </div>
+              <div id="copyright-text">
+                &copy; Reimu Inc. 2022. All rights reserved.
+              </div>
+            </div> */}
           </div>
         </div>
 
- 
+
 
       </footer>
     </div>
